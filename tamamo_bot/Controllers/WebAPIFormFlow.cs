c754a3af552ac98fd48a4d8e771047ea09ac15f8
zyusou <zyusou@live.jp>
2016-07-30 20:46:58 +0900

    first commit

---
+++ tamamo_bot/Controllers/WebAPIFormFlow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.FormFlow;
+
+
+namespace tamamo_bot.Controllers
+{
+    public enum CommandOptions
+    {
+        niconiko,
+        hatebu,
+        pixiv,
+        Amazon,
+        train
+    }
+
+    [Serializable]
+    public class WebAPIFormFlow
+    {
+        public CommandOptions? Sandwich;
+
+        public static IForm<WebAPIFormFlow> BuildForm()
+        {
+            return new FormBuilder<WebAPIFormFlow>()
+                    .Message("あら!ご主人様!何について調べますか？")
+                    .Build();
+        }
+    };
+}
---
+++ tamamo_bot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tamamo_bot.WebApiApplication" Language="C#" %>
